b9d4a0066cdd020d2a7920667f53c9da1088fcf0
GiorgiKapanadze777 <129076743+GiorgiKapanadze777@users.noreply.github.com>
2023-11-12 22:33:43 +0400

    Add files via upload

---
+++ ToDoList/main.js
@@ -0,0 +1,36 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+
+addTaskButton.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox">
+            <span>${taskText}</span>
+            <button class="delete">x</button>
+        `;
+
+        taskList.appendChild(li);
+        taskInput.value = "";
+    } else {
+        alert("Enter a task");
+    }
+});
+
+taskList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) {
+        event.target.parentElement.remove();
+    } else if (event.target.classList.contains("checkbox")) {
+        const task = event.target.nextElementSibling;
+
+        if (event.target.checked) {
+            task.classList.add("complete");
+        } else {
+            task.classList.remove("complete");
+        }
+    }
+});
